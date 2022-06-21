e918d64680c44b424d24bf6deb1191822058cab3
Jas-F <jasminer.franklin@gmail.com>
2022-06-21 12:39:08 -0400

    add find all

--- src/main/java/com/reavture/dao/EmployeeDao.java
+++ src/main/java/com/reavture/dao/EmployeeDao.java
@@ -1,19 +1,24 @@
 package com.reavture.dao;
 
-import org.hibernate.Session;
+import com.revature.models.Employee;
+import com.revature.util.HibernateUtil;
 
 public class EmployeeDao {
 
 	public int insert(Employee e) {
 		
-		Session ses = hibernateUtil.getSession();
+		Session ses = HibernateUtil.getSession();
 		
 		Transaction tx = ses.beginTransaction();	
 		
 		int pk = (int) ses.save(e);
 	
 	public List<Employee> findAll() {
+		Session ses = HibernateUtil.getSession();
 		
+		List<Employee> emps = ses.CreateQueey("from Employee", Employee.class).list();
+		
+		return emps;
 	}
 	
 	public boolean delete() {
